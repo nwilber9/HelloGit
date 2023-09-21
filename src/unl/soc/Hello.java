75877e389ec2505cf7c447af77c90d9dc2a68ae2
Noah M Wilber <60706568+noahw608@users.noreply.github.com>
2023-09-21 16:10:07 +0000

    Added name and major and added header

--- src/unl/soc/Hello.java
+++ src/unl/soc/Hello.java
@@ -1,6 +1,14 @@
+/**
+ * Noah Wilber nwilber9@huskers.unl.edu
+ * 9/21/23
+ */
+
 class Hello {
     public static void main(String[] args){
         System.out.println("Boah Bilbur");
         System.out.println("Engineering Computer");
+
+        System.out.println("Noah Wilber");
+        System.out.println("Computer Engineering")
     }
 }
