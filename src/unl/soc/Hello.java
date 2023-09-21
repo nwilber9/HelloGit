56bf09aba9c89198c6c6b577e66e3cabafe77a4e
nwilber9 <142922865+nwilber9@users.noreply.github.com>
2023-09-21 16:06:36 +0000

    Added Major

--- src/unl/soc/Hello.java
+++ src/unl/soc/Hello.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main(String[] args){
-        System.out.println("Boah Bilbur")
+        System.out.println("Boah Bilbur");
+        System.out.println("Engineering Computer");
     }
 }
